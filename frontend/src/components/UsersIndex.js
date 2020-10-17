e09f8deb1808db94c11f0cb0767772b185513e90
KathyPuma <kathypuma96@gmail.com>
2020-10-17 19:49:24 -0400

    Fix type

--- frontend/src/components/UsersIndex.js
+++ frontend/src/components/UsersIndex.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { apiURL } from '../util/apiURL'
 import axios from 'axios'
+import { AuthContext } from '../providers/AuthContext'
 export default function UsersIndex() {
     const [users, setUsers] = useState([])
     const API = apiURL()
+    const { token } = useContext(AuthContext)
 
     useEffect(() => {
         const fetchUser = async () => {
             let res = await axios({
                 method: 'get',
-                url: `${API}/api/users`
+                url: `${API}/api/users`,
+                headers: {
+                    'AuthToken': token
+                }
             })
             setUsers(res.data.users)
         }
@@ -21,7 +26,7 @@ export default function UsersIndex() {
 
     return (
         <div>
-            <h1>All users. Ypu are seeing this if you are logged in </h1>
+            <h1>All users. You are seeing this if you are logged in </h1>
             <ul>
                 {users.map(user => {
                     return <li key={user.id}> {user.email}</li>
