026e4e99279990123da6a63f7c20bcca78ab20ca
KathyPuma <kathypuma96@gmail.com>
2020-10-17 19:27:41 -0400

    Update Navbar to show logout button when a user is logged in and show login and signup link when no user is logged in

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import '../CSS/NavBar.css'
+import { logout } from '../util/firebaseFunctions'
+import { AuthContext } from '../providers/AuthContext'
 
 export default function NavBar() {
+    const { currentUser } = useContext(AuthContext)
+
+    const displayButton = () => {
+        if (currentUser) {
+            return <button onClick={logout}>Logout</button>
+
+        } else {
+            return (
+                <>
+                    <NavLink to={'/login'}>Log In</NavLink>
+                    <NavLink to={'/signup'}>Sign Up</NavLink>
+                </>
+            )
+        }
+    }
     return (
         <nav>
             <NavLink exact to={'/'}>Home</NavLink>
             <NavLink to={'/users'}>Show All Users</NavLink>
-            <NavLink to={'/login'}>Log In</NavLink>
-            <NavLink to={'/signup'}>Sign Up</NavLink>
-
+            {displayButton()}
         </nav>
     )
 }
